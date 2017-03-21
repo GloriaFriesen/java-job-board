3eaaec1f68de2e6273d2fe76db113ab052a7131c
Gloria Friesen and Stormi Hoebelheinrich <gloria@>
2017-03-21 15:34:00 -0700

    add getters for Job opening title, description, and contact info, tests for getter methods

--- src/main/java/JobOpening.java
+++ src/main/java/JobOpening.java
@@ -1,11 +1,22 @@
 public class JobOpening {
   private String mJobTitle;
+  private String mJobDescription;
+  private String mContactInfo;
 
-  public JobOpening(String jobTitle){
+  public JobOpening(String jobTitle, String jobDescription, String contactInfo){
     mJobTitle = jobTitle;
+    mJobDescription = jobDescription;
+    mContactInfo = contactInfo;
   }
 
   public String getJobTitle(){
     return mJobTitle;
+  }
+
+  public String getJobDescription(){
+    return mJobDescription;
+  }
+  public String getContactInfo(){
+    return mContactInfo;
   }
 }
--- src/test/java/JobOpeningTest.java
+++ src/test/java/JobOpeningTest.java
@@ -4,13 +4,25 @@ import static org.junit.Assert.*;
 public class JobOpeningTest{
   @Test
   public void JobOpening_instantiatesCorrectly_true() {
-    JobOpening newJobOpening = new JobOpening("Developer");
+    JobOpening newJobOpening = new JobOpening("Developer", "Must Be Able to Code", "Bossy Boss");
     assertEquals(true, newJobOpening instanceof JobOpening);
   }
 
   @Test
-  public void JobOpening_instantiatesCorrectly_jobTitleString() {
-    JobOpening newJobOpening = new JobOpening("Developer");
+  public void JobOpening_getsJobTitle_jobTitleString() {
+    JobOpening newJobOpening = new JobOpening("Developer", "Must Be Able to Code", "Bossy Boss");
     assertEquals("Developer", newJobOpening.getJobTitle());
+  }
+
+  @Test
+  public void JobOpening_getsJobDescription_jobDescriptionString() {
+    JobOpening newJobOpening = new JobOpening("Developer", "Must Be Able to Code", "Bossy Boss");
+    assertEquals("Must Be Able to Code", newJobOpening.getJobDescription());
+  }
+
+  @Test
+  public void JobOpening_getContactInfo_contactInfoString(){
+    JobOpening newJobOpening = new JobOpening("Developer", "Must Be Able to Code", "Bossy Boss");
+    assertEquals("Bossy Boss", newJobOpening.getContactInfo());
   }
 }
