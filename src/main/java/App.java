a7684a5ee7d6c4956a8fdecb5e4d6cbe4ab57a38
Gloria Friesen and Stormi Hoebelheinrich <gloria@>
2017-03-21 16:17:16 -0700

    add routes to app.java, add index and jobList templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,32 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    ArrayList<JobOpening> jobsArray = new ArrayList<JobOpening>();
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/jobList", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      request.session().attribute("jobsArrayKey", jobsArray);
+      String title = request.queryParams("jobTitle");
+      String description = request.queryParams("jobDescription");
+      String contact = request.queryParams("jobContactInfo");
+      JobOpening newJob = new JobOpening(title, description, contact);
+      jobsArray.add(newJob);
+
+      model.put("printJobs", request.session().attribute("jobsArrayKey"));
+      model.put("template", "templates/jobList.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
